acd1778f7eebcdd40f096e89ab363c962bd0ce22
mauropetrarulo <m.petrarulo@gmail.com>
2022-08-13 13:48:20 -0400

    adding fbxs

---
+++ Assets/SCRIPTS/CAR.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CAR : MonoBehaviour
+{
+
+    public WheelCollider wheelColliderLeftFront;
+    public WheelCollider wheelColliderRightFront;
+    public WheelCollider wheelColliderLeftBack;
+    public WheelCollider wheelColliderRightBack;
+
+    public Transform wheelLeftFront;
+    public Transform wheelRightFront;
+    public Transform wheelLeftBack;
+    public Transform wheelRightBack;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
